673992855347323b96206821d90e5da8bd5f0c48
gitHub <cyc00201@gmail.com>
2020-03-04 10:56:45 +0800

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by User06 on 2020/3/4.
+//  Copyright © 2020 matcha55. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
